db97664a30aced0125425140179546c8e5205d61
LostGeneration1999 <masaya4426saganosen@gmail.com>
2020-12-30 17:00:05 +0900

    Front-Back接続完了

--- server/main.py
+++ server/main.py
@@ -1,20 +1,29 @@
-from flask import Flask
-from flask import request, make_response, jsonify
-from flask_cors import CORS
+from flask import Flask, render_template, jsonify, request, make_response
+from flask_restful import Api, Resource
+from flask_cors import CORS 
 
 
 app = Flask(__name__, static_folder="./build/static", template_folder="./build")
+
+app.config['JSON_AS_ASCII'] = False
 CORS(app)
+
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  return response
 
 @app.route("/", methods=['GET'])
 def index():
-    return "text parser)"
+    return "Server OK"
 
 @app.route('/tags', methods=['GET', 'POST'])
 def parse():
     data = request.get_json()
     text = data['post_tags']
-    response = {'result': res}
+    response = {'result': text}
     return make_response(jsonify(response))
 
 if __name__ == "__main__":
